bf5fffde99c591cd78c8fa10a9af6356e1b2d161
Jimmy Ossa <runninggator94@gmail.com>
2022-10-11 13:46:42 -0400

    Add a basic bar chart component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <BarChart />
   </div>
 </template>
+
+<script>
+import BarChart from "@/components/BarChart";
+
+export default {
+  name: "AboutView",
+  components: { BarChart },
+};
+</script>
